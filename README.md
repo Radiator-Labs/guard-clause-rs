dae861ac60ecdfee86e717724e53b7005af86701
shakencodes <shakencodes@gmail.com>
2024-12-11 14:16:03 -0800

    Basic guard macro functionality

---
+++ README.md
@@ -0,0 +1,80 @@
+# Guard-Clause [![Cloud-CI](https://github.com/Radiator-Labs/guard-clause/actions/workflows/cloud-ci.yml/badge.svg)](https://github.com/Radiator-Labs/guard-clause/actions/workflows/cloud-ci.yml)
+
+Guard-clause library, providing syntactic sugar to improve readability of
+[guard-clauses](https://en.wikipedia.org/wiki/Guard_(computer_science))
+in Rust code.
+
+## Intention
+
+The Rust programming language provides the [`assert`](https://doc.rust-lang.org/core/macro.assert.html)
+macro in the core library (starting in 1.6.0, assert was originally in the standard library.)
+The `assert` allows values to be checked and, if not as desired, panics the application.
+Guard-clauses attempts to deliver a similar functionality, but returns a value instead of panicking.
+
+Original Code:
+
+```rust
+if (value > threshold) {
+    return Err(Error::OverThreshold);
+}
+```
+
+Code with guard-clause:
+
+```rust
+guard!(value <= threshold, Err(Error::OverThreshold));
+```
+
+This expresses the guard clause in a more concise manner, improving readability.
+
+### Development goals
+
+The guard-clause is intended to have no dependencies and operate in no-std code
+bases. (Dev-dependencies will be allowed, if necessary.)
+
+Pedantic clippy is used to drive a more idiomatic codebase.
+
+### Other options
+
+It has also been discussed to make the guard-clause even more tailored, with the following options:
+
+* Return a Result::Error, as `guard_err(value <= threshold, Error::OverThreshold);`
+  * Perhaps this should be the default, or only option.
+* Return an Option::None, as `guard_none(value <= threshold);`
+* Return a Result::Error<&'static str>, as `guard_str(value <= threshold, "Value is over threshold");`
+  * Should this return a String instead, and use a formatter to insert values in the String?
+
+The current thinking is that these options do not offer enough benefit over the version
+that just returns a generic type.
+
+## Attribution
+
+Work creating this library was performed as part of commercial development
+by [Kelvin](https://kel.vin/) (formerly Radiator Labs), a green energy company
+dedicated to decarbonizing the world's legacy buildings.
+
+## Minimum Supported Rust Version (MSRV)
+
+This crate is guaranteed to compile on stable Rust 1.83 and up. It *might*
+compile with older versions but that may change in any new patch release.
+Changes in Clippy support are the main factor driving the version dependency.
+
+See [here](../docs/msrv.md) for details on how the MSRV may be upgraded.
+
+## License
+
+Licensed under either of
+
+* Apache License, Version 2.0 ([LICENSE-APACHE](LICENSE-APACHE) or
+  <http://www.apache.org/licenses/LICENSE-2.0>)
+* MIT license ([LICENSE-MIT](LICENSE-MIT) or <http://opensource.org/licenses/MIT>)
+
+at your option.
+
+### Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in the work by you, as defined in the Apache-2.0 license, shall be
+dual licensed as above, without any additional terms or conditions.
+
+## end
---
+++ src/lib.rs
@@ -0,0 +1,116 @@
+/// Checks that a boolean expression is `true` at runtime, otherwise performs
+/// a return with the second parameter, short-circuiting the operation of the
+/// enclosing function.
+///
+/// # Uses
+///
+/// Guard clauses are used, generally at the start of functions, to ensure that
+/// conditions are valid for the operation of the function. The usage is similar
+/// to an assertion, but the guard clause returns a value that may be evaluated
+/// and acted on by the calling function, while assertions end the program by
+/// panicking.
+///
+/// The guard macro is syntactic sugar that replaces an if clause that would
+/// return when true. The guard macro can be written on one line, while the
+/// default Rust code formatter expands any if clause to three lines.
+///
+/// # Examples
+///
+/// ```rust
+/// use guard_clause::guard;
+///
+/// fn safe_divide_using_option(lval: i32, rval: i32) -> Option<i32> {
+///     guard!(rval != 0, None);
+///     Some(lval / rval)
+/// }
+/// assert_eq!(Some(3), safe_divide_using_option(6, 2));
+/// assert_eq!(None, safe_divide_using_option(6, 0));
+///
+/// fn safe_divide_using_result_str(lval: i32, rval: i32) -> Result<i32, &'static str> {
+///     guard!(rval != 0, Err("Divide by zero!"));
+///     Ok(lval / rval)
+/// }
+/// assert_eq!(Ok(3), safe_divide_using_result_str(6, 2));
+/// assert_eq!(Err("Divide by zero!"), safe_divide_using_result_str(6, 0));
+///
+/// #[derive(Debug, PartialEq)]
+/// enum DivError {
+///     DivideByZero
+/// }
+/// fn safe_divide_using_result_enum(lval: i32, rval: i32) -> Result<i32, DivError> {
+///     guard!(rval != 0, Err(DivError::DivideByZero));
+///     Ok(lval / rval)
+/// }
+/// assert_eq!(Ok(3), safe_divide_using_result_enum(6, 2));
+/// assert_eq!(Err(DivError::DivideByZero), safe_divide_using_result_enum(6, 0));
+/// ```
+#[macro_export]
+macro_rules! guard {
+    ($check:expr, $fail_return:expr) => {
+        if !$check {
+            return $fail_return;
+        }
+    };
+}
+
+#[cfg(test)]
+mod tests {
+    fn use_guard_with_result(test: bool, guard_fail: TestError) -> Result<(), TestError> {
+        guard!(test, Err(guard_fail));
+        Ok(())
+    }
+    #[test]
+    fn given_a_true_condition_guard_with_result_return_type_then_ok_with_enum() {
+        assert_eq!(use_guard_with_result(true, TestError::Reason2), Ok(()));
+    }
+    #[test]
+    fn given_a_false_condition_guard_guard_with_result_return_type_then_err_with_enum() {
+        let expected_err = TestError::Reason2;
+        assert_eq!(
+            use_guard_with_result(false, expected_err),
+            Err(expected_err)
+        );
+    }
+
+    fn use_guard_with_option(test: bool) -> Option<()> {
+        guard!(test, None);
+        Some(())
+    }
+    #[test]
+    fn given_a_true_condition_guard_with_option_return_type_then_ok_with_enum() {
+        assert_eq!(use_guard_with_option(true), Some(()));
+    }
+    #[test]
+    fn given_a_false_condition_guard_guard_with_option_return_type_then_err_with_enum() {
+        assert_eq!(use_guard_with_option(false), None);
+    }
+
+    fn use_guard_with_string_slice(test: bool, guard_fail: &'static str) -> &'static str {
+        guard!(test, guard_fail);
+        GOOD_STRING_SLICE
+    }
+    #[test]
+    fn given_a_true_condition_guard_with_string_slice_return_type_then_ok_with_enum() {
+        assert_eq!(
+            use_guard_with_string_slice(true, BAD_STRING_SLICE),
+            GOOD_STRING_SLICE
+        );
+    }
+    #[test]
+    fn given_a_false_condition_guard_guard_with_string_slice_return_type_then_err_with_enum() {
+        assert_eq!(
+            use_guard_with_string_slice(false, BAD_STRING_SLICE),
+            BAD_STRING_SLICE
+        );
+    }
+
+    #[derive(Debug, PartialEq, Clone, Copy)]
+    enum TestError {
+        _Reason1,
+        Reason2,
+        _Reason3(&'static str),
+    }
+
+    const GOOD_STRING_SLICE: &str = "Good!";
+    const BAD_STRING_SLICE: &str = "bad...";
+}
